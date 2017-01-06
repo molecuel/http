1193fe3080ae669dd05e0e4524c1bc6a8b3bb8f5
dominicboettger <dominic.boettger@inspirationlabs.com>
2017-01-06 14:06:02 +0100

    Added initial unit tests

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 'use strict';
 import 'reflect-metadata';
+import {di, singleton} from '@molecuel/di';
+import * as Koa from 'koa';
+
+@singleton
+export class MlclKoa extends Koa {}
--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,30 @@
 'use strict';
 import 'reflect-metadata';
-import should = require('should');
+import assert = require('assert');
+import should =  require('should');
+import {di, injectable} from '@molecuel/di';
+import {MlclCore} from '@molecuel/core';
+import {MlclKoa} from '../dist';
 should();
 
-describe('MlclHttp', function() {
+describe('MlclCoreInit', function() {
+  it('should bootstrap', function() {
+    di.bootstrap(MlclCore, MlclKoa);
+  });
+  it('should init molecuel core', function() {
+    let core: MlclCore = di.getInstance('MlclCore');
+    assert(core !== undefined);
+    assert(core instanceof MlclCore);
+  });
+});
 
+describe('MlclKoa', function() {
+  it('should return a MlclKoa instance', function() {
+    let koa = di.getInstance('MlclKoa');
+    assert(koa !== undefined);
+    assert(koa instanceof MlclKoa);
+  });
+  it('should be a singleton instance', function() {
+    assert(di.getInstance('MlclKoa') === di.getInstance('MlclKoa'));
+  });
 }); // test end
